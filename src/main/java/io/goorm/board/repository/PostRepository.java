24df665e8b14fd78eb311d490db22e899838d65a
jung0522 <jjy1111202@naver.com>
2025-09-01 10:59:46 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/io/goorm/board/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package io.goorm.board.repository;
+
+
+import io.goorm.board.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
